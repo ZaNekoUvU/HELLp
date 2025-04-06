454ac90b922ed3e666efb667d96e9464add49b24
ZaNekoUvU <angusvaneeden@gmail.com>
2025-04-06 14:02:34 +0200

    SomeUI changes

--- Assets/Scripts/SoulPickup.cs
+++ Assets/Scripts/SoulPickup.cs
@@ -2,17 +2,6 @@ using UnityEngine;
 
 public class SoulPickup : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     //When the Soul collides with player
     private void OnCollisionEnter2D(Collision2D other)
     {
