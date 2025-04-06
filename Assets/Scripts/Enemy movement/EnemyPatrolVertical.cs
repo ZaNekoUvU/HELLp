91fa659d73ffcb08be8c89526e285590321dec44
NdzalamaKhosa <st10438552@vgconnect.edu.za>
2025-04-06 15:43:31 +0200

    fixed enemy anims

--- Assets/Scripts/Enemy movement/EnemyPatrolVertical.cs
+++ Assets/Scripts/Enemy movement/EnemyPatrolVertical.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class EnemyPatrolVertical : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float moveSpeed = 1f;
 
-    // Update is called once per frame
+    private bool movingUp = true;
+
     void Update()
     {
-        
+        if (movingUp)
+        {
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        movingUp = !movingUp;
+        Flip();
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.y *= -1; // Flip Y
+        transform.localScale = localScale;
     }
 }
