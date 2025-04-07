77d6b1d77f9ef258606e4789063fc795e3b3fa90
RiyaadIsaacs <166004664+RiyaadIsaacs@users.noreply.github.com>
2025-04-07 16:07:11 +0200

    working on boss script
    scripting the souls and the boss

--- Assets/Scripts/SoulPickup.cs
+++ Assets/Scripts/SoulPickup.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class SoulPickup : MonoBehaviour
 {
+    [SerializeField] private int soul;
+    [SerializeField] private GameObject boss;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,11 +13,23 @@ public class SoulPickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (soul >= 10)
+        {
+            if (GameObject.Find("Boss") == null)
+            {
+                boss = Instantiate(boss);
+                boss.transform.position = GameObject.Find("Boss Spawner").transform.position;
+
+            }
+        }
     }
     //When the Soul collides with player
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Object.Destroy(this.gameObject);
+        if (other.gameObject.CompareTag("SoulFragment"))
+        {
+            soul++;
+            Object.Destroy(other.gameObject);
+        }
     }
 }
